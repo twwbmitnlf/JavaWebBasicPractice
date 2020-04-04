273d49449ad6013ad01b94d3f3627fdc6fbe6031
11954 <1195470375@qq.com>
2020-04-04 18:39:24 +0800

    demo

---
+++ src/com/lq/prc/HelloWorld.java
@@ -0,0 +1,31 @@
+package com.lq.prc;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class HelloWorld extends javax.servlet.http.HttpServlet {
+    private String message;
+
+    public void init() throws ServletException {
+        // 执行必需的初始化
+        message = "Hello World111";
+    }
+
+    public void doGet(HttpServletRequest request,
+                      HttpServletResponse response)
+            throws ServletException, IOException {
+        // 设置响应内容类型
+        response.setContentType("text/html");
+
+        // 实际的逻辑是在这里
+        PrintWriter out = response.getWriter();
+        out.println("<h1>" + message + "</h1>");
+    }
+
+    public void destroy() {
+        // 什么也不做
+    }
+}
